893a36b8bab02b80b9268513bc396ee97a6290e4
Grant Eadie <grantleadie@gmail.com>
2020-08-10 16:26:14 -0700

    Convert sentence to first and last letter capitalized and reversed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  jQuery("img").click(function() {
-    alert("This is an image.");
-  });
+  function first(sentence) {
+    return (sentence.charAt(0));
+  }
+  function last(sentence) {
+    return (sentence.charAt(sentence.length-1));
+  }
+  const sentence = prompt("Write a sentence.");
+  const result2 = first(sentence.toUpperCase());
+  const result1 = last(sentence.toUpperCase());
+  alert(result1 + result2);
 });
