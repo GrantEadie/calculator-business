9810e87860de52489b6abaddb84401e3b0d76c5d
Grant Eadie <grantleadie@gmail.com>
2020-08-10 16:57:02 -0700

    capitalize new characters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 function firstLast(sentence) {
-  return sentence.charAt(0) + sentence.charAt(sentence.length-1)
+  return sentence.charAt(0) + sentence.charAt(sentence.length-1);
+}
+
+function capitalize(sentence) {
+  return (firstLast(sentence)).toUpperCase();
 }
 
 const sentence = prompt("Enter a sentence: ");
-alert(firstLast(sentence));
+alert(capitalize(sentence));
